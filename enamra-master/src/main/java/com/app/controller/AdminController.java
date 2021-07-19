779cdabf59ecc1aeeebb5a9bb2524c500e467d0d
tringuyen1032 <trinmse140981@fpt.edu.vn>
2021-07-19 17:05:26 +0700

    - create question by excel

--- enamra-master/src/main/java/com/app/controller/AdminController.java
+++ enamra-master/src/main/java/com/app/controller/AdminController.java
@@ -1,10 +1,19 @@
 package com.app.controller;
 
 
+import com.app.model.Answer;
+import com.app.model.Quiz;
 import com.app.model.User;
 import com.app.service.AdminService;
+import com.app.service.AnswerService;
+import com.app.service.QuizService;
 import com.app.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +21,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.apache.poi.ss.usermodel.Row;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.*;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
@@ -33,6 +47,12 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private QuizService quizService;
+
+    @Autowired
+    private AnswerService answerService;
 
     @Autowired
     private AdminService adminService;
@@ -60,7 +80,6 @@ public class AdminController {
         ModelAndView model = new ModelAndView();
         model.setViewName("admin/create_question");
         return model;
-
     }
 
     @PostMapping("/signup")
@@ -127,5 +146,139 @@ public class AdminController {
 
         }
         return "admin/edit";
+    }
+
+    @PostMapping("create_by_file")
+    public String createByFile(Model model, MultipartFile file) throws IOException {
+        byte[] bytes = file.getBytes();
+        String name = file.getOriginalFilename();
+        Path path = Paths.get("./src/main/resources/static/upload_files/" + name);
+        try {
+            Files.write(path, bytes);
+        } catch (FileSystemException ex) {
+            ex.printStackTrace();
+        }
+        List<Quiz> listQuizs = readQuizsFromExcelFile(path.toString());
+        for (Quiz quiz: listQuizs) {
+            quizService.saveQuiz(quiz);
+        }
+        List<Answer> listAnss = readAnsersFromExcelFile(path.toString());
+        for (Answer answer: listAnss) {
+            answerService.saveAnswer(answer);
+        }
+        return "admin/create_question";
+    }
+
+    public List<Quiz> readQuizsFromExcelFile(String excelFilePath) throws IOException {
+        List<Quiz> listQuizs = new ArrayList<Quiz>();
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+        Workbook workBook = getWorkbook(inputStream, excelFilePath);
+        Sheet firstSheet = workBook.getSheetAt(0);
+        Iterator<Row> rows = firstSheet.iterator();
+
+        while (rows.hasNext()) {
+            Row row = rows.next();
+            Iterator<Cell> cells = row.cellIterator();
+            Quiz quiz = new Quiz();
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                int columnIndex = cell.getColumnIndex();
+
+                switch (columnIndex) {
+                    case 0:
+                        quiz.setQuestionID(Integer.parseInt((getCellValue(cell)+"")));
+                        break;
+                    case 1:
+                        quiz.setQuestion((String) getCellValue(cell));
+                        break;
+                    case 2:
+                        quiz.setStatus((String) getCellValue(cell));
+                        break;
+                    case 3:
+                        quiz.setSubjectID((String) getCellValue(cell));
+                        break;
+                }
+            }
+            long millis=System.currentTimeMillis();
+            quiz.setCreateDate(new java.sql.Date(millis));
+            listQuizs.add(quiz);
+        }
+
+        workBook.close();
+        inputStream.close();
+
+        return listQuizs;
+    }
+
+    public List<Answer> readAnsersFromExcelFile(String excelFilePath) throws IOException {
+        List<Answer> listAnss = new ArrayList<Answer>();
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+        Workbook workBook = getWorkbook(inputStream, excelFilePath);
+        Sheet firstSheet = workBook.getSheetAt(0);
+        Iterator<Row> rows = firstSheet.iterator();
+
+        while (rows.hasNext()) {
+            Row row = rows.next();
+            Iterator<Cell> cells = row.cellIterator();
+            Answer answer = new Answer();
+            String listanswers[]=null;
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                int columnIndex = cell.getColumnIndex();
+                switch (columnIndex) {
+                    case 0:
+                        answer.setQuestionID(Integer.parseInt((getCellValue(cell)+"")));
+                        break;
+                    case 4:
+                        String answers = (String) getCellValue(cell);
+                        listanswers = answers.split(",");
+                        break;
+                    case 5:
+                        answer.setStatus((boolean) getCellValue(cell) );
+                        break;
+                }
+            }
+            for (String ans: listanswers) {
+                Answer x = new Answer();
+                x.setQuestionID(answer.getQuestionID());
+                x.setAnswer(ans);
+                x.setStatus(answer.isStatus());
+                listAnss.add(x);
+            }
+        }
+        workBook.close();
+        inputStream.close();
+        return listAnss;
+    }
+
+    private Workbook getWorkbook(FileInputStream inputStream, String excelFilePath) throws IOException {
+        Workbook workbook = null;
+
+        if (excelFilePath.endsWith("xlsx")) {
+            workbook = new XSSFWorkbook(inputStream);
+        } else if (excelFilePath.endsWith("xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        } else {
+            throw new IllegalArgumentException("The specified file is not Excel file");
+        }
+
+        return workbook;
+    }
+
+    private Object getCellValue(Cell cell) {
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_STRING:
+                return cell.getStringCellValue();
+
+            case Cell.CELL_TYPE_BOOLEAN:
+                return cell.getBooleanCellValue();
+
+            case Cell.CELL_TYPE_NUMERIC:
+                return cell.getNumericCellValue();
+        }
+
+        return null;
     }
 }
